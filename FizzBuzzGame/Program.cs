ddc80f8e74c39e4e5e03151868bf4a45ec80a14d
Dev-Data-Net <dev.data.net@gmail.com>
2024-05-11 10:09:06 +0200

    Week 4 lesson 13 - homework

--- FizzBuzzGame/Program.cs
+++ FizzBuzzGame/Program.cs
@@ -7,12 +7,18 @@ namespace FizzBuzzGame
         static void Main(string[] args)
         {
             Console.WriteLine("Witaj serdecznie w grze FizzBuzz");
+            Console.WriteLine("\nW grze należy podać dowolną liczbę.");
+            Console.WriteLine("\nJeżeli twoja liczba będzie podzielna przez 3 bez reszty, to otrzymasz wynik 'Fizz'.");
+            Console.WriteLine("Jak będzie podzielna przez 5 bez reszty, to otrzymasz wynik 'Buzz'.");
+            Console.WriteLine("W przypadku jak będzie podzielna przez 3 i przez 5 jednocześnie bez reszty, to otrzymasz wynik 'FizzBuzz'.");
+            Console.WriteLine("Jeżeli jednak liczba nie będzie podzielna przez 3 ani przez 5 bez reszty, to wtedy otrzymasz zwróconą liczbę.");
+            Console.WriteLine("ZACZYNAMY!");
             var FizzBuzz = new FizzBuzz();
 
 
             while (true)
             {
-                Console.WriteLine("Podaj dowolną liczbę: ");
+                Console.WriteLine("\nPodaj dowolną liczbę: ");
                 var number = GetNumber();
                 Console.WriteLine(FizzBuzz.GetOut(number));
                 Console.WriteLine();
